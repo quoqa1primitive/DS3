d106093dbbc2f96802509bd9827fbbf45d8b39ee
AELGyeom <ksg_0320@kaist.ac.kr>
2022-03-14 15:02:29 +0900

    Label added, Texts Visible option unified Bar length fixed Quiz Button(Not Page) added

--- src/Components/ImmVisComponent.js
+++ src/Components/ImmVisComponent.js
@@ -12,6 +12,11 @@ const STEP_XY = 100;
 const STEP_ZY = 101;
 const STEP_ZX = 110;
 
+const XAXIS1 = 1000;
+const YAXIS1 = 1001;
+const YAXIS2 = 1010;
+const ZAXIS1 = 1011;
+
 const scale = 5.5;
 const tickLength = 0.6;
 const speed = 0.035;
@@ -26,9 +31,9 @@ const opts = {
   materialType: "MeshPhongMaterial"
 };
 const xyzProps = {
-  xSteps: 2,  xLength: 57.33,   xPadding: 15,
-  ySteps: 11, yLength: 54.398,  yPadding: 0,
-  zSteps: 3,  zLength: 100,     zPadding: 10,
+  xSteps: 2,  xLength: 60,  xPadding: 15,
+  ySteps: 11, yLength: 60,  yPadding: 0,
+  zSteps: 3,  zLength: 100, zPadding: 10,
   dataA1: [30, 60, 70, 80, 90, 130, 140, 150, 150, 170, 180, 210],
   dataB1: [90, 90, 100, 100, 110, 120, 120, 140, 140, 150, 160, 180],
   dataA2: [60, 60, 60, 55, 55, 55, 50, 50, 50, 45, 40, 40],
@@ -50,16 +55,25 @@ function Line({ start, end, color }) {
   )
 }
 
-function TextBox({opts, text, anchorX, anchorY}){
+function TextBox({text, anchorX, anchorY, label}){
   const ref = useRef();
   useFrame((state) => {
     ref.current.lookAt(new THREE.Vector3(state.camera.position.x, state.camera.position.y, state.camera.position.z));
+    if(label == XAXIS1){
+      // ref.current.rotateX(Math.PI / 2);
+    }else if(label == YAXIS1){
+      ref.current.rotateZ(Math.PI / 2);
+    }else if(label == YAXIS2){
+      ref.current.rotateZ(Math.PI / 2);
+    }else if(label == ZAXIS1){
+
+    }
   })
 
   return(
     <text {...opts} ref={ref} text={text + ""} font={fonts[opts.font]} anchorX={anchorX} anchorY={anchorY}>
       {opts.materialType === "MeshPhongMaterial" ? (
-        <meshPhongMaterial attach="material" color={opts.color} />
+        <meshPhongMaterial attach="material" color={opts.color} side={THREE.DoubleSide} />
       ) : null}
     </text>
   )
@@ -86,7 +100,9 @@ function Rect({ width, height, depth, color, opacity }){
   )
 }
 
-function AxGr({position}){
+function AxGr({step, position}){
+  console.log(step);
+
   const xLength = xyzProps.xLength, yLength = xyzProps.yLength, zLength = xyzProps.zLength;
   const xPadding = xyzProps.xPadding, yPadding = xyzProps.yPadding, zPadding = xyzProps.zPadding;
   const xSteps = xyzProps.xSteps, ySteps = xyzProps.ySteps, zSteps = xyzProps.dataA1.length;
@@ -96,11 +112,27 @@ function AxGr({position}){
       {
         Array(xSteps).fill(0).map((x, y) => x + y).map((item, idx) => {
           return <mesh key={idx} position={[xPadding + item * ((xLength - 2 * xPadding) / (xSteps - 1)), -tickLength, zLength]}>
-              <Line key={idx} color={"black"} start={[0, 0, 0]} end={[0, tickLength, 0]} /> // Tick
-              <TextBox opts={opts} text={String.fromCharCode(65+item)} anchorX={"center"} anchorY={"top"} /> // Label
-              <Line key={idx+100} color={"lightgrey"} start={[0, tickLength, 0]} end={[0, tickLength, -zLength]} /> // Grid
+              {
+                (step <= 2) &&
+                <>
+                  <Line key={idx} color={"black"} start={[0, 0, 0]} end={[0, tickLength, 0]} /> // Tick
+                  <TextBox text={String.fromCharCode(65+item)} anchorX={"center"} anchorY={"top"} /> // Label
+                </>
+              }
+              {
+                (step <= 4) &&
+                <Line key={idx+100} color={"lightgrey"} start={[0, tickLength, 0]} end={[0, tickLength, -zLength]} /> // Grid
+              }
             </mesh>
         })
+      }
+      {
+        (step <= 2) &&
+        <>
+          <group position={[xLength / 2, -6, zLength]}>
+            <TextBox text={"City"} anchorX={"center"} anchorY={"bottom"} label={XAXIS1}/>
+          </group>
+        </>
       }
     </>
 
@@ -109,12 +141,57 @@ function AxGr({position}){
     {
       Array(ySteps).fill(0).map((x, y) => x + y).map((item, idx) => {
         return <mesh key={idx} position={[-tickLength, item * ((yLength - 2 * yPadding) / (ySteps - 1)), 0]}>
-            <Line key={idx} color={"black"} start={[0, 0, 0]} end={[tickLength, 0, 0]} /> // Tick
-            <TextBox opts={opts} text={3 + 3 * item} anchorX={"right"} anchorY={"middle"} /> // Label
-            <Line key={idx+100} color={"lightgrey"} start={[tickLength, 0, 0]} end={[xLength, 0, 0]} /> // Grid
-            <Line key={idx+200} color={"lightgrey"} start={[xLength, 0, 0]} end={[xLength, 0, zLength]} /> // Grid
+            {
+              (step >= 2 && step <= 4) &&
+              <>
+                <Line key={idx} color={"black"} start={[0, 0, 0]} end={[tickLength, 0, 0]} /> // Tick
+                <TextBox text={0 + 3 * item} anchorX={"right"} anchorY={"middle"} /> // Label
+              </>
+            }
+            {
+              (step <= 5) &&
+              <Line key={idx+100} color={"lightgrey"} start={[tickLength, 0, 0]} end={[xLength, 0, 0]} /> // Grid
+            }
+            {
+              (step >= 2 && step <= 5) &&
+              <Line key={idx+200} color={"lightgrey"} start={[xLength, 0, 0]} end={[xLength, 0, zLength]} /> // Grid
+            }
           </mesh>
       })
+    }
+    {
+      (step >= 2 && step <= 4) &&
+      <>
+        <group position={[-6, yLength / 2, -6]}>
+          <TextBox text={"Meat Consumption(ton)"} anchorX={"center"} anchorY={"bottom"} label={YAXIS1}/>
+        </group>
+      </>
+    }
+  </>
+
+  const YAxis2 =
+  <>
+    {
+      Array(ySteps).fill(0).map((x, y) => x + y).map((item, idx) => {
+        return <mesh key={idx} position={[yPadding + item * ((yLength - 2 * yPadding) / (ySteps - 1)), -tickLength, -tickLength]}>
+            {
+              (step >= 6) &&
+              <>
+                <Line key={idx} color={"black"} start={[0, 0, tickLength]} end={[0, 0, 0]} /> // Tick
+                <TextBox text={0 + 10 * item} anchorX={"right"} anchorY={"middle"} /> // Label
+                <Line key={idx+100} color={"lightgrey"} start={[0, 0, 0]} end={[0, 0, zLength]} /> // Grid
+              </>
+            }
+          </mesh>
+      })
+    }
+    {
+      (step >= 6) &&
+      <>
+        <group position={[xLength / 2, 0, -6]}>
+          <TextBox text={"Food Self-Sufficiency(%)"} anchorX={"center"} anchorY={"bottom"} label={YAXIS2}/>
+        </group>
+      </>
     }
   </>
 
@@ -123,11 +200,24 @@ function AxGr({position}){
     {
       Array(xyzProps.dataA1.length).fill(0).map((x, y) => x + y).map((item, idx) => {
         return <mesh key={idx} position={[0, -tickLength, zPadding + item * ((zLength - 2 * zPadding) / (zSteps - 1))]}>
-            <Line key={idx} color={"black"} start={[0, 0, 0]} end={[-tickLength, 0, 0]} /> // Tick
-            <TextBox opts={opts} text={1 + 1 * item} anchorX={"center"} anchorY={"top"} /> // Label
+            {
+              (step >= 4) &&
+              <>
+                <Line key={idx} color={"black"} start={[0, 0, 0]} end={[0, tickLength, 0]} /> // Tick
+                <TextBox text={1 + 1 * item} anchorX={"center"} anchorY={"top"} /> // Label
+              </>
+            }
             <Line key={idx+100} color={"lightgrey"} start={[0, tickLength, 0]} end={[xLength, tickLength, 0]} /> // Grid
           </mesh>
       })
+    }
+    {
+      (step >= 4) &&
+      <>
+        <group position={[-6, -6, zLength / 2]}>
+          <TextBox text={"Month"} anchorX={"center"} anchorY={"bottom"} label={ZAXIS1}/>
+        </group>
+      </>
     }
   </>
 
@@ -136,10 +226,20 @@ function AxGr({position}){
       {XAxis1}
       {YAxis1}
       {ZAxis1}
-      <Line color={"black"} start={[0, 0, zLength]} end={[xLength, 0, zLength]} /> // X-Axis
-      <Line color={"black"} start={[0, 0, 0]} end={[0, yLength, 0]} /> // Y-Axis
+      {YAxis2}
+      {
+        (step <= 4) &&
+        <>
+          <Line color={"black"} start={[0, 0, zLength]} end={[xLength, 0, zLength]} /> // X-Axis
+          <Line color={"black"} start={[0, 0, 0]} end={[0, yLength, 0]} /> // Y-Axis
+        </>
+      }
       <Line color={"black"} start={[0, 0, 0]} end={[0, 0, zLength]} /> // Z-Axis
-      // <Line color={"black"} start={[0, 0, 0]} end={[xLength, 0, 0]} /> // Z-Axis2
+      {
+        (step >= 4) &&
+        <Line color={"black"} start={[0, 0, 0]} end={[xLength, 0, 0]} /> // X-Axis2
+      }
+
     </group>
   )
 }
@@ -183,6 +283,7 @@ function MainGroup2({position, target}){
   const xPadding = xyzProps.xPadding, yPadding = xyzProps.yPadding, zPadding = xyzProps.zPadding;
   const xSteps = xyzProps.xSteps, ySteps = xyzProps.ySteps, zSteps = xyzProps.dataA1.length;
   const rectDepth = 2;
+  const ratio = 1.66;
 
   return(
     <group position={position}>
@@ -191,10 +292,10 @@ function MainGroup2({position, target}){
           return <mesh
             key={idx}
             position={[
-              0 + xyzProps.dataA2[idx] / 2,
+              0 + xyzProps.dataA2[idx] / ratio /2,
               0,
               zPadding + idx * ((zLength - 2 * zPadding) / (zSteps - 1)) - rectDepth / 2]}>
-              <Rect width={xyzProps.dataA2[idx]} height={item} depth={rectDepth} color={new THREE.Color("rgb(0, 255, 0)")} opacity={idx==xyzProps.dataA1.length-1?1:1}/>
+              <Rect width={xyzProps.dataA2[idx] / ratio} height={item} depth={rectDepth} color={new THREE.Color("#512C8A")} opacity={idx==xyzProps.dataA1.length-1?1:1}/>
             </mesh>
         })
       }
@@ -203,10 +304,10 @@ function MainGroup2({position, target}){
           return <mesh
             key={idx}
             position={[
-              0 + xyzProps.dataB2[idx] / 2,
+              0 + xyzProps.dataB2[idx] / ratio / 2,
               0,
               zPadding + idx * ((zLength - 2 * zPadding) / (zSteps - 1)) + rectDepth / 2]}>
-              <Rect width={xyzProps.dataB2[idx]} height={item} depth={rectDepth} color={new THREE.Color("rgb(255, 0, 0)")} opacity={idx==xyzProps.dataB1.length-1?1:1}/>
+              <Rect width={xyzProps.dataB2[idx] / ratio} height={item} depth={rectDepth} color={new THREE.Color("#2F9B39")} opacity={idx==xyzProps.dataB1.length-1?1:1}/>
             </mesh>
         })
       }
@@ -216,37 +317,53 @@ function MainGroup2({position, target}){
 
 function VisComponent({camera, scroll, ...props}){
   const group = useRef();
+  const [step, setStep] = useState(0);
 
-  let step = 0;
-  const sp_1 = 0.2;
-  const sp_2 = 0.48;
+  const sp_1 = 0.14;
+  const sp_2 = 0.38;
+  const sp_3 = 0.64;
 
   useFrame(() => {
     const et = scroll.current;
     // console.log(et);
 
-    group.current.position.x = -xyzProps.xLength * 3/4;
+    // group.current.position.x = -xyzProps.xLength * 3/4;
 
     let bezierFunc = bezier(0.4, 0, 0.4, 1);
     let bzVal = 0;
     const durMargin = 0.03;
 
     if(et < (sp_1 - durMargin)){
-      step = 1;
+      setStep(1);
       bzVal = bezierFunc((et - 0) / (sp_1 - durMargin));
       group.current.rotation.y = -Math.PI / 6 * (1 - bzVal);
       camera.current.position.y = 2000 * (1 - bzVal);
     }else if(et < (sp_1 + durMargin)){
-      step = 2;
+      setStep(2);
       group.current.rotation.y = 0;
       camera.current.position.y = 0;
     }else if(et < (sp_2 - durMargin)){
-      step = 3;
+      setStep(3);
       let et2 = et - sp_1 - durMargin;
-      let dur = sp_2 - durMargin - sp_1 - durMargin;
-      bzVal = bezierFunc(et2 / dur);
+      let dur2 = sp_2 - durMargin - sp_1 - durMargin;
+      bzVal = bezierFunc(et2 / dur2);
       group.current.rotation.y = Math.PI / 2 * bzVal;
       camera.current.position.y = 500 * Math.sin(bzVal * Math.PI);
+    }else if(et < (sp_2 + durMargin)){
+      setStep(4);
+      group.current.rotation.y = Math.PI / 2;
+      camera.current.position.y = 500 * Math.sin(bzVal * Math.PI);
+    }else if(et < (sp_3 - durMargin)){
+      setStep(5);
+      let et3 = et - sp_2 - durMargin;
+      let dur3 = sp_3 - durMargin - sp_2 - durMargin;
+      bzVal = bezierFunc(et3 / dur3);
+      group.current.rotation.z = Math.PI / 2 * bzVal;
+      camera.current.position.x = -500 * Math.sin(bzVal * Math.PI);
+    }else if(et < (sp_3 + durMargin)){
+      setStep(6);
+      group.current.rotation.z = Math.PI / 2;
+      camera.current.position.x = 0;
     }
 
     camera.current.lookAt(0, 0, 0);
@@ -261,7 +378,14 @@ function VisComponent({camera, scroll, ...props}){
   return(
     <group ref={group}>
       <AxGr step={step} position={centerPos} xyzProps={xyzProps} />
-      <MainGroup1 position={centerPos} xyzProps={xyzProps} />
+      {
+        (step < 4) &&
+          <MainGroup1 position={centerPos} xyzProps={xyzProps} />
+      }
+      {
+        (step >= 4) &&
+          <MainGroup2 position={centerPos} xyzProps={xyzProps} />
+      }
     </group>
   )
 }
--- src/Components/Overlay.js
+++ src/Components/Overlay.js
@@ -2,20 +2,22 @@ import React, { useRef, useEffect, useState } from 'react';
 
 import './Overlay.css';
 
-const text1 = `육류 소비량은 증가하고 있다.
-A 도시는 농축산업 지원정책에 따른 식습관 변화였고,
-B 도시는 도시화의 진행에 따라 빠른 속도로 증가했다.
+const text1 = `올해 A, B 도시의 육류 소비량은 매우 크게 증가하였다.
+A 도시는 농축산업 지원정책에 따른,
+B 도시는 도시화에 따른 결과였다.
 `
 const text2 = `월별 소비량은 다음과 같으며,
-A 도시의 육류 소비량은 30t에서 210t으로,
-B 도시의 육류 소비량 또한 90t에서 180t으로 둘 다 꾸준한 상승세를 보였다.
+A 도시는 30t에서 210t으로, B 도시는 90t에서 180t으로
+둘 다 비교적 꾸준한 상승세를 보였다.
 `
-const text3 = `이에 반해 식량 자급자족률은 서로 상반된 결과를 가졌다.
+const text3 = `그렇다면 식량 자급자족률도 높아졌을까?
+결과는 상반되었다.
 A 도시는 30%에서 80%까지 치솟았고,
 B 도시는 60%에서 40%로 수치가 감소했다.
 `
-const text4 = `이를 통해, 도시화가 진행될 때는 주변에 농축산업 중점 지역을 형성하면
-높은 식량 자급자족률을 통해 주변 도시로의 원활한 공급을 기대할 수 있다.
+const text4 = `이를 극복하고자 한다면,
+도시화가 진행되는 지역 주변에 농축산업 중점 지역을 형성하여
+높은 식량 자급자족률을 통해 원활한 신선식품 공급을 기대할 수 있다.
   `
 
 function TextComponent(props){
@@ -37,7 +39,7 @@ const Overlay = React.forwardRef(({ scroll }, ref) => (
     }}>
     <div className="TitleContainer">
       <div className="Title">
-        식량 소비에 관하여
+        A, B 도시의 식량 소비
       </div>
     </div>
     <div className={"Texts"}>
@@ -45,9 +47,10 @@ const Overlay = React.forwardRef(({ scroll }, ref) => (
       <TextComponent text = {text2}/>
       <TextComponent text = {text3}/>
       <TextComponent text = {text4}/>
-    </div>
-    <div className={"QuizButton"}>
-
+      <button class="Button"
+              type="button">
+        Go to Quiz
+      </button>
     </div>
   </div>
 ))
